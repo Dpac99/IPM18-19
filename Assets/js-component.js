5d0df60abcf406cf39a05d0d3801b4c209f0dd09
Dpac99 <diogo.a.pacheco@tecnico.ulisboa.pt>
2019-02-24 23:03:59 +0000

    Click to see numbers

--- Assets/js-component.js
+++ Assets/js-component.js
@@ -1,3 +1,6 @@
-function toggleVisible(id) {    
-    
+function toggleVisible(id) {
+    let style;
+    let element = document.getElementById(id) 
+    element.style.visibility == "collapse"? style = "visible" : style = "collapse"
+    element.style.visibility = style
 }
