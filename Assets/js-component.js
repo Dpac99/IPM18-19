7b946f1d5dc3f47fa9012b89089a8ca0baf87551
Dpac99 <diogo.a.pacheco@tecnico.ulisboa.pt>
2019-03-26 18:33:36 +0000

    finished main menu and added myTrip menu

--- Assets/js-component.js
+++ Assets/js-component.js
@@ -4,3 +4,15 @@ function toggleVisible(id) {
     element.style.visibility == "collapse"? style = "visible" : style = "collapse"
     element.style.visibility = style
 }
+
+function unlock(){
+    document.location.href = "mainScreen1.html"
+}
+
+function myTrip(){
+    document.location.href = "myTripScreen.html"
+}
+
+function lock(){
+    document.location.href = "protFuncional1-beta.html"
+}
