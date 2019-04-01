51c51f5cd9ff0cfefc278143476000fc25a47108
Andreia <andreia.sofia.pereira@tecnico.ulisboa.pt>
2019-04-01 15:50:31 +0100

    Add ticket done

--- Prot1/Assets/js/tickets.js
+++ Prot1/Assets/js/tickets.js
@@ -7,48 +7,48 @@ var currentFlight = 0;
 var exp = new Vue({
     el: "#ticket",
     data: {
-        flights:[],
+        flights: [],
         qrcode: "Assets/images/frame.png",
         dots: []
     }
 })
 
 function init() {
-   for(i=0; i<flights.length; i++){
-       let current = {
-           company: flights[i].company,
-           itinerary: flights[i].itinerary,
-           number: flights[i].number,
-           seat: flights[i].seat,
-           date: flights[i].date,
-           time: flights[i].time
-       }
-       exp.flights.push(current)
-       if(i === currentFlight){
-           exp.dots.push({class: "full-dot"})
-       }
-       else{
-           exp.dots.push({class: "dot"})
-       }
-   }
+    for (i = 0; i < flights.length; i++) {
+        let current = {
+            company: flights[i].company,
+            itinerary: flights[i].itinerary,
+            number: flights[i].number,
+            seat: flights[i].seat,
+            date: flights[i].date,
+            time: flights[i].time
+        }
+        exp.flights.push(current)
+        if (i === currentFlight) {
+            exp.dots.push({ class: "full-dot" })
+        }
+        else {
+            exp.dots.push({ class: "dot" })
+        }
+    }
 }
 
 init()
 
 function updateDots() {
     exp.dots = []
-    for (i=0; i< flights.length; i++){
-        if(i===currentFlight){
-            exp.dots.push({class: "full-dot"})
+    for (i = 0; i < flights.length; i++) {
+        if (i === currentFlight) {
+            exp.dots.push({ class: "full-dot" })
         }
-        else{
-            exp.dots.push({class: "dot"})
+        else {
+            exp.dots.push({ class: "dot" })
         }
     }
 }
 
 function nextFlight() {
-    if(currentFlight === flights.length - 1){
+    if (currentFlight === flights.length - 1) {
         return
     }
     currentFlight++
@@ -60,7 +60,7 @@ function nextFlight() {
 }
 
 function prevFlight() {
-    if(currentFlight === 0){
+    if (currentFlight === 0) {
         return
     }
     currentFlight--
@@ -74,12 +74,16 @@ function prevFlight() {
 function addTicket() {
     scanner()
     setTimeout(scanner, 6000)
-    exp.flights.push({
-            company: "Ryanair ",
-            itinerary: "LIS- LDN",
-            number: "1148",
-            seat: "30F",
-            date: "25/06/2019",
-            time: "11:25",
+    flights.push({
+        company: "Ryanair ",
+        itinerary: "LIS- LDN",
+        number: "1148",
+        seat: "30F",
+        date: "25/06/2019",
+        time: "11:25",
     })
+    localStorage.setItem("flights", JSON.stringify(flights))
+    exp.flights = []
+    exp.dots = []
+    init()
 } 
